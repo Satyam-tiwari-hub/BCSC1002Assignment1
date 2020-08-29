9e3c5ea06ceac271e06e066105018275521369dd
ASUS <satyam.tiwari_cs19@gla.ac.in>
2020-08-29 18:10:43 +0530

    create parameterized constructor for Student.java

--- src/Student.java
+++ src/Student.java
@@ -5,4 +5,14 @@ public class Student {
     private long universityRollNumber;
     private int numberOfBooksIssuedByTheStudent;
     private String[] namesOfTheBooksIssuedByTheStudent;
+
+    public Student(String firstName, String middleName, String lastName, long universityRollNumber,
+                   int numberOfBooksIssuedByTheStudent, String[] namesOfTheBooksIssuedByTheStudent) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.universityRollNumber = universityRollNumber;
+        this.numberOfBooksIssuedByTheStudent = numberOfBooksIssuedByTheStudent;
+        this.namesOfTheBooksIssuedByTheStudent = namesOfTheBooksIssuedByTheStudent;
+    }
 }
