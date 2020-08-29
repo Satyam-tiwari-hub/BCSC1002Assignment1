d590a231d8e216da6c2e5c98e0cd4a0ffb0fe3f1
ASUS <satyam.tiwari_cs19@gla.ac.in>
2020-08-29 18:01:50 +0530

    add private fields to Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,8 @@
 public class Student {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private long universityRollNumber;
+    private int numberOfBooksIssuedByTheStudent;
+    private String[] namesOfTheBooksIssuedByTheStudent;
 }
