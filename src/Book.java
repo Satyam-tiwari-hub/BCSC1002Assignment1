992d12bbb749ff807ff3b0d3ca3f527345b39937
ASUS <satyam.tiwari_cs19@gla.ac.in>
2020-08-29 11:57:52 +0530

    create a constructor of Book.java

--- src/Book.java
+++ src/Book.java
@@ -2,4 +2,10 @@ public class Book {
     private String bookName;
     private String authorName;
     private long ISBN_NUMBER;
+
+    public Book() {
+        bookName = null;
+        authorName = null;
+        ISBN_NUMBER = 0;
+    }
 }
